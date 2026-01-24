121d1d2cfc39c675a6ef89ef9e57932183d5cd8f
Mahdy Arief <mahdyarief@gmail.com>
2026-01-24 10:34:14 +0700

    rename temp

--- src/collections/user/users_temp.ts
+++ src/collections/user/users_temp.ts
@@ -1,5 +1,5 @@
-import type { CollectionConfig } from 'payload'
 import { groups } from '@/lib/groups'
+import type { CollectionConfig } from 'payload'
 export const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
